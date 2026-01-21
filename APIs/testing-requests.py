97851dab4541e81385fea2bd67cc10ad34f015e5
etucoder <ethu2020m@gmail.com>
2026-01-21 17:27:14 -0600

    Figured out rquests

--- APIs/testing-requests.py
+++ APIs/testing-requests.py
@@ -1,4 +1,5 @@
 import requests
 print("IS THIS EVEN WORKING!!!!!!!!!!!")
-response = requests.get("https://cat-fact.herokuapp.com/fact")
-print(response)
+response = requests.get("https://catfact.ninja/fact")
+data = response.json()
+print(data)
